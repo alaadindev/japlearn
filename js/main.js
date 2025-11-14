a227b63e4eea630af5ddd84141c36dbf0be047ce
alaadev <alaadindev@gmail.com>
2025-11-14 11:19:20 +0200

    added showlist

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 import CardDom from './dom/card.js'
 import Store from './store/store.js'
 let upload = document.getElementById("upload")
-let upload_btn = upload.getElementsByTagName("button")[0]
-let upload_file = upload.getElementsByTagName("input")[0]
+let saveBtn = upload.getElementsByTagName("button")[0]
+let uploadFile = document.getElementById("uploadfile")
 let loadedfile = document.getElementById("loadedfile")
 
 const store = new Store()
 
-upload_file.addEventListener('change', load)
+uploadFile.addEventListener('change', load)
+saveBtn.addEventListener('click', save)
+
 
 function load(){
-    let filename = upload_file.files[0]
+
+    let filename = uploadFile.files[0]
     if (!filename) return
 
     const reader = new FileReader()
@@ -43,6 +46,11 @@ function display_loaded_file(){
     })
     loadedfile.innerHTML = ""
     loadedfile.appendChild(cards)
+    let showinfo = document.getElementById('showinfo')
+    let addshow = document.getElementById('addshow')
+    addshow.classList.add('hidden')
+    showinfo.classList.add('hidden')
+    loadedfile.classList.remove('hidden')
 }
 function play_sound(e){
     let id = (e.currentTarget.id).split('-')[0]
@@ -89,14 +97,111 @@ async function translate_text(e){
         translationElem.innerText = value
         content.insertBefore(translationElem, control)
         card.classList.add("viewed")
-        console.log(value, translationElem)
     }catch(err){
         console.log(err)
     }
 }
 
-function loadfile(){
-
+function save(){
+    showSaveForm()
 }
+function showSaveForm(){
+    loadedfile.classList.add('hidden')
+    let form = document.getElementById('addshow')
+    form.innerHTML = ""
+    form.classList.remove('hidden')
+    let add = document.createElement('button')
+    add.innerText = "add"
+    add.addEventListener('click', addShow)
+    let show = document.createElement('input')
+    show.setAttribute('type', 'text')
+    show.setAttribute('placeholder', 'show')
+    show.id = 'show'
+    let season = document.createElement('input')
+    season.setAttribute('type', 'text')
+    season.setAttribute('placeholder', 'season')
+    season.id = 'season'
+    let episode = document.createElement('input')
+    episode.setAttribute('type', 'text')
+    episode.setAttribute('placeholder', 'episode')
+    episode.id = 'episode'
+
+    form.appendChild(show)
+    form.appendChild(season)
+    form.appendChild(episode)
+    form.appendChild(add)
+    console.log(store.getShows())
+}
+function addShow(){
+    let show = document.getElementById('show')
+    let season = document.getElementById('season')
+    let episode = document.getElementById('episode')
+    let showinfo = document.getElementById('showinfo')
+    let addshow = document.getElementById('addshow')
+    showinfo.innerHTML = ""
+    showinfo.classList.add('hidden')
+    addshow.innerHTML = ""
+    addshow.classList.add('hidden')
+    store.addShow(show.value, season.value, episode.value)
+    display_loaded_file()
+}
+function displayShows(){
+    let shows = store.getShows()
+    console.log(shows)
+    if (!shows) return
+    let showslist = document.getElementById('showslist')
+    shows.forEach(show => {
+        let li = document.createElement('li')
+        li.innerText = show
+        li.addEventListener('click', (e)=>displayShow(show))
+        showslist.appendChild(li)
+    });
+}
+function displayShow(show){
+    let seasons = store.getSeasons(show)
+    let showinfo = document.getElementById('showinfo')
+    showinfo.classList.remove('hidden')
+    showinfo.innerHTML = ""
+    let seasoninfo = document.getElementById('seasoninfo')
+    seasoninfo.innerHTML = ""
+    seasoninfo.classList.add('hidden')
+    seasons.forEach(season => {
+        let li = document.createElement('li')
+        li.innerText = "season "+season
+        li.addEventListener('click', (e)=>{displaySeason(season, show)})
+        showinfo.appendChild(li)
+    });
+    console.log(seasons)
+}
+function displaySeason(season, show){
+    console.log(season)
+    let episodes = store.getEpisodes(show, season)
+    let seasoninfo = document.getElementById('seasoninfo')
+    seasoninfo.innerHTML = ""
+    seasoninfo.classList.remove('hidden')
+    console.log(episodes)
+    episodes.forEach(episode => {
+        let li = document.createElement('li')
+        li.innerText = episode
+        li.addEventListener('click', (e)=>{displaySubtitiles(show, season, episode)})
+        seasoninfo.appendChild(li)
+        console.log(episode)
+    });
+}
+function displaySubtitiles(show, season, episode){
+    let showinfo = document.getElementById('showinfo')
+    showinfo.classList.add('hidden')
+    let seasoninfo = document.getElementById('seasoninfo')
+    seasoninfo.classList.add('hidden')
+    let filekey = `${show}-${season}-${episode}`
+    let file = store.get(filekey)
+    store.setCurrent(file)
+    let loadedfile = document.getElementById('loadedfile')
+    display_loaded_file()
+    loadedfile.classList.remove('hidden')
+    console.log(file)
+}
+showSaveForm()
+displayShows()
 display_loaded_file()
 
--- js/store/store.js
+++ js/store/store.js
@@ -8,6 +8,53 @@ export default class Store{
     get(value){
         return JSON.parse(localStorage.getItem(value))
     }
+    getShows(){
+        let keys = this.getkeys()
+        let shows = []
+        for (let i =0; i< keys.length; i++){
+            let show = keys[i].split('-')[0]
+            if (!shows.includes(show)){
+                shows.push(show)
+            }
+        }
+        return shows
+    }
+    getSeasons(show){
+        let keys = this.getkeys()
+        let seasons = []
+        for (let i =0; i< keys.length; i++){
+            let info = keys[i].split('-')
+            if (show==info[0]){
+                if (!seasons.includes(info[1])){
+                    seasons.push(info[1])
+                }
+            }
+        }
+        console.log(seasons)
+        return seasons.sort()
+    }
+    getEpisodes(show, season){
+        let keys = this.getkeys()
+        let episodes = []
+        for (let i=0; i<keys.length;i++){
+            let info = keys[i].split('-')
+            if (show == info[0] && season == info[1] && !episodes.includes(info[2])){
+                episodes.push(info[2])
+            }
+        }
+        return episodes.sort()
+    }
+    getkeys(){
+        let keys = []
+        for (let i=0;i < localStorage.length; i++){
+            keys.push(localStorage.key(i))
+        }
+        return keys.filter(key => key !='loaded')
+    }
+    addShow(show, season, episode){
+        let key = `${show}-${season}-${episode}`
+        this.set(key, this.getCurrent())
+    }
     setCurrent(value){
         this.set('loaded', value)
     }
